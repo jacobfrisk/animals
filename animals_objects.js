7f50b6910f6f550f052f12503b8707f91e5ee669
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-09-04 15:21:40 +0200

    init

---
+++ animals_objects.js
@@ -0,0 +1,62 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const allAnimals = [];
+
+function start() {
+  console.log("ready");
+
+  loadJSON();
+}
+
+function loadJSON() {
+  fetch("animals.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(jsonData) {
+  jsonData.forEach((jsonObject) => {
+    // TODO: Create new object with cleaned data - and store that in the allAnimals array
+    // Fullname strengen splittes op i sit eget array
+    const newArray = jsonObject.fullname.split(" ");
+
+    // Der laves et nyt objekt hvor værdier fra det nye array tilgås ved hjælp af index
+    const animal = {
+      name: newArray[0],
+      type: newArray[3],
+      desc: newArray[2],
+      age: jsonObject.age,
+    };
+
+    // Man bruger push for at tilføje de nye objekter til allAnimals arrayet der bruges længere nede
+    allAnimals.push(animal);
+  });
+  displayList();
+}
+
+function displayList() {
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
+
+  // build a new list
+  allAnimals.forEach(displayAnimal);
+}
+
+function displayAnimal(animal) {
+  // create clone
+  const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
+
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
+}
