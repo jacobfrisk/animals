a1d3c3057af16cccb6f99721950bb88e856592bc
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-09-05 15:16:56 +0200

    filter done

--- animals_objects.js
+++ animals_objects.js
@@ -1,3 +1,86 @@
+// "use strict";
+
+// window.addEventListener("DOMContentLoaded", start);
+
+// const allAnimals = [];
+
+// function start() {
+//   loadJSON();
+// }
+
+// function loadJSON() {
+//   fetch("animals.json")
+//     .then((response) => response.json())
+//     .then((jsonData) => {
+//       // when loaded, prepare objects
+//       prepareObjects(jsonData);
+//     });
+// }
+
+// function prepareObjects(jsonData) {
+//   jsonData.forEach((jsonObject) => {
+//     // Fullname strengen splittes op i sit eget array
+//     const newArray = jsonObject.fullname.split(" ");
+
+//     // Der laves et nyt objekt hvor værdier fra det nye array tilgås ved hjælp af index
+//     const animal = {
+//       name: newArray[0],
+//       type: newArray[3],
+//       desc: newArray[2],
+//       age: jsonObject.age,
+//     };
+
+//     // Man bruger push for at tilføje de nye objekter til allAnimals arrayet der bruges længere nede
+//     allAnimals.push(animal);
+//   });
+//   displayList();
+// }
+
+// function displayList() {
+//   // clear the list
+//   document.querySelector("#list tbody").innerHTML = "";
+
+//   // build a new list
+//   allAnimals.forEach(displayAnimal);
+// }
+
+// function displayAnimal(animal) {
+//   // create clone
+//   const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+//   // set clone data
+//   clone.querySelector("[data-field=name]").textContent = animal.name;
+//   clone.querySelector("[data-field=desc]").textContent = animal.desc;
+//   clone.querySelector("[data-field=type]").textContent = animal.type;
+//   clone.querySelector("[data-field=age]").textContent = animal.age;
+
+//   // append clone to list
+//   document.querySelector("#list tbody").appendChild(clone);
+// }
+
+// //Lave alle filtreringsknapper til variabler
+// const allAnimalsFilterButton = document.querySelector("#all");
+// const onlyCatsFilterButton = document.querySelector("#cats");
+// const onlyDogsFilterButton = document.querySelector("#dogs");
+
+// //Henter alle td
+
+// // Her er koden der kun får de elektriske frem
+// onlyCatsFilterButton.addEventListener("click", () => {
+//   const table = document.querySelector("#list tbody");
+//   const onlyCat = allAnimals.filter(isCat);
+//   table.textContent = "";
+//   displayList(onlyCat);
+// });
+
+// function isCat(animal) {
+//   if (animal.type === "cat") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
 "use strict";
 
 window.addEventListener("DOMContentLoaded", start);
@@ -5,8 +88,6 @@ window.addEventListener("DOMContentLoaded", start);
 const allAnimals = [];
 
 function start() {
-  console.log("ready");
-
   loadJSON();
 }
 
@@ -14,48 +95,63 @@ function loadJSON() {
   fetch("animals.json")
     .then((response) => response.json())
     .then((jsonData) => {
-      // when loaded, prepare objects
       prepareObjects(jsonData);
     });
 }
 
 function prepareObjects(jsonData) {
   jsonData.forEach((jsonObject) => {
-    // Fullname strengen splittes op i sit eget array
     const newArray = jsonObject.fullname.split(" ");
-
-    // Der laves et nyt objekt hvor værdier fra det nye array tilgås ved hjælp af index
     const animal = {
       name: newArray[0],
       type: newArray[3],
       desc: newArray[2],
       age: jsonObject.age,
     };
-
-    // Man bruger push for at tilføje de nye objekter til allAnimals arrayet der bruges længere nede
     allAnimals.push(animal);
   });
   displayList();
 }
 
-function displayList() {
-  // clear the list
+// Changed the displayList function to accept an optional 'animals' parameter
+function displayList(animals = allAnimals) {
   document.querySelector("#list tbody").innerHTML = "";
-
-  // build a new list
-  allAnimals.forEach(displayAnimal);
+  animals.forEach(displayAnimal);
 }
 
 function displayAnimal(animal) {
-  // create clone
   const clone = document.querySelector("template#animal").content.cloneNode(true);
-
-  // set clone data
   clone.querySelector("[data-field=name]").textContent = animal.name;
   clone.querySelector("[data-field=desc]").textContent = animal.desc;
   clone.querySelector("[data-field=type]").textContent = animal.type;
   clone.querySelector("[data-field=age]").textContent = animal.age;
-
-  // append clone to list
   document.querySelector("#list tbody").appendChild(clone);
+}
+
+const allAnimalsFilterButton = document.querySelector("#all");
+const onlyCatsFilterButton = document.querySelector("#cats");
+const onlyDogsFilterButton = document.querySelector("#dogs");
+
+onlyCatsFilterButton.addEventListener("click", () => {
+  // Filter only cat entries and display them
+  const onlyCat = allAnimals.filter(isCat);
+  displayList(onlyCat);
+});
+
+onlyDogsFilterButton.addEventListener("click", () => {
+  const onlyDog = allAnimals.filter(isDog);
+  displayList(onlyDog);
+});
+
+allAnimalsFilterButton.addEventListener("click", () => {
+  // Display the full list of animals when "All" button is clicked
+  displayList();
+});
+
+function isCat(animal) {
+  return animal.type === "cat";
+}
+
+function isDog(animal) {
+  return animal.type === "dog";
 }
